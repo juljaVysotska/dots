a1440b24a84b8717a019af0d53c81ab9fb06fb05
Julja Vysotska <julja.vysotska@gmail.com>
2023-06-21 01:16:23 +0300

    add form section

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssMin = require('gulp-cssmin');
+
+function buildHtml() {
+    return src('./app/*.html')
+    .pipe(dest('./src/'))
+}
+
+function buildImg() {
+    return src('./app/img/**/*.*')
+    .pipe(dest('./src/img/'))
+}
+
+function buildStyles() {
+    return src('./app/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(dest('./src/styles'));
+}
+
+
+function browserSyncInit() {
+    return browserSync.init({
+        server: {
+            baseDir: "./src/"
+        }
+    });
+}
+
+function browserSyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+function watchTask() {
+    watch('./app/*.html', series(buildHtml, browserSyncReload));
+    watch(['./app/**/*.scss'], series(buildStyles, browserSyncReload));
+}
+exports.scss = buildStyles;
+exports.html = watchTask;
+
+exports.default = series(
+    buildHtml, 
+    buildImg,
+    buildStyles,
+    parallel(
+        browserSyncInit,
+        watchTask
+        )
+);
